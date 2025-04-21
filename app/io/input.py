f3468fd2bd2a7764c2911acffe59baf3a5ac5f8c
illia <illia.antypenko@gmail.com>
2025-04-21 18:15:39 +0300

    Add empty I/O functions

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def read_console():
+    pass
+
+def read_file(path):
+    pass
+
+def read_file_pandas(path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def write_console():
+    pass
+
+def write_file(path):
+    pass
